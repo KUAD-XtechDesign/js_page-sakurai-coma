dda454efda117574079d98cdd079909848530b13
sakurai-coma <s-sakurai0271@st.kyoto-art.ac.jp>
2021-11-11 05:05:17 +0900

    a

--- js/script.js
+++ js/script.js
@@ -1,64 +1,56 @@
 $(function(){
-
-    let windowH
-    let documentH 
-    let documentW 
-    let scrollTop
-    let separate
-    let scrollRatio
-  
-    //スクロールしたりウィンドウサイズを変えた時に実行
-    $(window).on("scroll resize load",function(){
-      windowH = $(this).height()//ウィンドウの高さ
-      documentH = $(document).height()//内容の高さ
-      documentW = $(document).width()//内容の幅
-      scrollTop = $(this).scrollTop()//何ピクセルスクロールしたか
-      scrollRatio = scrollTop/(documentH-windowH)//どれくらいスクロールされたか0から1
-      
-      separate = documentH / 3;//内容の高さを3分割した数値
-  
-      //３分割した数値とどれだけスクロールしたかを比較→bodyにクラスをつける。あとはCSSファイルでデザイン変更
-      if(scrollTop < separate){
-        $("body").removeClass().addClass("one")
-      }else if(scrollTop < separate * 2){
-        $("body").removeClass().addClass("two")
-      }else{
-        $("body").removeClass().addClass("three")
-      }
-  
-      console.log(windowH,documentH,scrollTop)
-  
-      $("#wave").css("background-position-x",scrollTop/10)//右上の背景画像の位置変更(CSS)
-      $("#line").css("width",scrollRatio * documentW)//グラデーションラインの幅変更(CSS)
-  
-    })
-  
-  
-    //Menuボタンを押した時
-    $("#btn01").on("click",function(){
-      $("html, body").animate({scrollTop:0}, 1000, "swing");
-    })
-  
-    $("#btn02").on("click",function(){
-      $("html, body").animate({scrollTop:separate*1}, 1000, "swing");
-    })
-  
-    $("#btn03").on("click",function(){
-      $("html, body").animate({scrollTop:separate*2}, 1000, "swing");
-    })
+	
+    var imgWidth = $('.sample img').width();
+    var imgHeight = $('.sample img').height();
     
-    $("#btn04").on("click",function(){
-        $("html, body").animate({scrollTop:separate*3}, 1000, "swing");
-      })
+    aspectRatio = imgWidth / imgHeight
 
-    $("#btn05").on("click",function(){
-        $("html, body").animate({scrollTop:separate*4}, 1000, "swing");
-      })
+    if(aspectRatio >= 1){
+        //横長画像の場合 divのheightに数値を合わせる
+        $('.sample img').css('height','200px');
+    }else{
+        //縦長画像の場合 divのwidthに数値を合わせる
+        $('.sample img').css('width','300px');
 
-    $("#btn06").on("click",function(){
-        $("html, body").animate({scrollTop:separate*5}, 1000, "swing");
-      })
+        //上下中央揃えにする場合は下記2行も
+        var i = (imgHeight-200)/2  //はみ出た部分を計算して÷2し、ネガティブマージンをつける
+        $(this).find('img').css('margin-top', '-'+i+'px');
+
+    }
+
+
+});
+jQuery(function($){
     
-  
-  
-  })
+    $('.slider').slick({
+        arrows:false,
+        asNavFor:'.thumb',
+    });
+    $('.thumb').slick({
+        asNavFor:'.slider',
+        focusOnSelect: true,
+        slidesToShow:4,
+        slidesToScroll:1
+    });    
+});
+
+
+$('.slider').slick({
+    arrows:false,
+    asNavFor:'.thumb',
+});
+$('.thumb').slick({
+    asNavFor:'.slider',
+    focusOnSelect: true,
+    slidesToShow:4,
+    slidesToScroll:1
+});    
+jQuery('.grid').masonry({
+    itemSelector: '.grid-item'
+  });
+
+  var $grid = jQuery('.grid').imagesLoaded(function(){
+	$grid.masonry({
+		itemSelector: '.grid-item'
+  });
+});
